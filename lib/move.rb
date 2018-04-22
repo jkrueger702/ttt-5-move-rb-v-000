d276134debf69e788402c778864cf8c530c65609
Jordan Krueger <jkrueger702@gmail.com>
2018-04-22 02:17:40 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,10 +8,24 @@ end
 
 input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def input_to_index(input)
-index = input.to_i - 1
+index = input.to_i + 1
 index
 end
 
 def move(board, input, character = "X")
   board[input] = character
+end
+
+def greeting
+  puts "Welcome to Tic Tac Toe!"
+end
+
+def question
+  puts "Where would you like to go?"
+end
+
+def board
+  board = 'board'
+  board = Array.new
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
